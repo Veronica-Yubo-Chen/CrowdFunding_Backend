6ea5ed5125e372527b4885f177bf629e6fbfcecc
Yubo (Veronica) Chen <Yubo.Chen@riotinto.com>
2025-11-15 10:16:04 +1000

    Add API views and routing for fundraisers app
    Introduces FundraiserList and FundraiserDetail API views for listing, creating, and retrieving fundraiser objects. Updates main URL configuration to include fundraisers app URLs, enabling RESTful endpoints for fundraisers.

---
+++ crowdfunding/fundraisers/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.FundraiserList.as_view(), name='fundraiser-list'),
+    path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
+]
--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Fundraiser
+from .serializers import FundraiserSerializer
+from rest_framework import status
+from django.http import Http404
 
-# Create your views here.
+class FundraiserList(APIView):
+    
+    def get(self, request):
+        fundraiser = Fundraiser.objects.all()
+        serializer = FundraiserSerializer(fundraiser, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = FundraiserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED 
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+        
+class FundraiserDetail(APIView):
+    def get_object(self, pk):
+        try:
+            fundraiser = Fundraiser.objects.get(pk=pk)
+            return fundraiser
+        except Fundraiser.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        fundraiser = self.get_object(pk)
+        serializer = FundraiserSerializer(fundraiser)
+        return Response(serializer.data)
